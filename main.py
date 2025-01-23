c90de9dfff574308287fe37b8f714074de6a9c8a
otabek-a <otabekabdurasulov245@gmail.com>
2025-01-23 20:03:10 +0500

    edit count like/dislike

--- main.py
+++ main.py
@@ -14,6 +14,43 @@ def info(update: Update, context):
     inline_keyboard = InlineKeyboardMarkup([[inline_button,inline_button1]])
     update.message.reply_photo(photo=file_id,caption='do you like/dislike ?',reply_markup=inline_keyboard)
 
+
+
+
+def button_callback(update:Update,context):
+    query=update.callback_query
+    query.answer()
+    if query.data=='like':
+        button_count['like']+=1
+    elif query.data=='dislike':
+        button_count['dislike']+=1
+    
+    inline_button = InlineKeyboardButton(text=f"👍 {button_count['like']}",callback_data='like')
+    inline_button1 = InlineKeyboardButton(text=f"👎 {button_count['dislike']}",callback_data='dislike')
+    inline_keyboard = InlineKeyboardMarkup([[inline_button,inline_button1]])
+
+
+    query.edit_message_reply_markup(reply_markup=inline_keyboard)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def start(update: Update, context):
     update.message.reply_text("Hey please send photo for work this bot")
 
@@ -30,6 +67,6 @@ dispatcher = updater.dispatcher
 dispatcher.add_handler(MessageHandler(Filters.photo, info))
 
 dispatcher.add_handler(CommandHandler('start',start))
-
+dispatcher.add_handler(CallbackQueryHandler(button_callback))
 updater.start_polling()
 updater.idle()
